22bceb30582ad5036b4a951cf37b3a3b652e69b7
Rajeetjoshi <joshirajit28@gmail.com>
2025-09-16 13:10:00 +0530

    Update handlers.rs

--- handlers.rs
+++ handlers.rs
@@ -4,7 +4,7 @@ use uuid::Uuid;
 
 use crate::models::{User, CreateUser};
 
-// Get all users
+
 pub async fn get_users(pool: web::Data<PgPool>) -> HttpResponse {
     let users = sqlx::query_as::<_, User>("SELECT * FROM users")
         .fetch_all(pool.get_ref())
@@ -14,7 +14,6 @@ pub async fn get_users(pool: web::Data<PgPool>) -> HttpResponse {
     HttpResponse::Ok().json(users)
 }
 
-// Create a new user
 pub async fn create_user(user: web::Json<CreateUser>, pool: web::Data<PgPool>) -> HttpResponse {
     let new_user = sqlx::query_as::<_, User>(
         "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id, name, email"
@@ -28,4 +27,3 @@ pub async fn create_user(user: web::Json<CreateUser>, pool: web::Data<PgPool>) -
     HttpResponse::Created().json(new_user)
 }
 
-// You can similarly implement get_user, update_user, delete_user using SQL queries.
